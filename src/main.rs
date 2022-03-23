96c8cac56f598df0eb66c15f5c42a0b540af4a9c
JakWai01 <jakob.waibel@web.de>
2022-03-23 17:02:21 +0100

    feat: Work on sequential implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,72 @@
+use std::env;
+use std::fs;
+use std::fmt;
+use std::collections::HashMap;
+
+struct KVStore<T, U> {
+    key: T, value: U,
+}
+
+impl<T, U> KVStore<T, U> {
+    fn new(key: T, value: U) -> Self {
+        KVStore { key,
+            value,
+        }
+    }
+}
+
 fn main() {
     println!("Hello, world!");
+    
+    let args: Vec<String> = env::args().collect();    
+    println!("{:?}", args);
+
+    //
+    // read each input file,
+    // pass it to Map,
+    // accumulate the intermediate Map output.
+    //
+    let mut intermediate: HashMap<&str,&str> = HashMap::new();
+    // intermediate.insert("key", "value");
+    for file in &args[1..] {
+        println!("{:?}", &file);
+        let contents = fs::read_to_string(file)
+            .expect(format!("Could not read file: {}", stringify!(&file)).as_str());
+
+        println!("{}", &contents);
+        // kva := mapf(file, contents)
+        // intermediate = append(intermediate, kva...) 
+        map(&file, &contents)
+    }
+    
+    println!("{:?}", intermediate);
+
+    // 
+    // a big difference from real MapReduce is that all the
+    // intermediate data is in one place, intermediate[],
+    // rather than being partinioned into NxM buckets.
+    //
+
+    //
+    // call Reduce on each distinct key in intermediate[],
+    // and print the result to mr-out-0.
+    //
+}
+
+//
+// The map function is called once for each file of input. The first
+// argument is the name of the input file, and the second is the
+// file's complete contents. You should ignore the input file name,
+// and look only at the contents argument. The return value is a slice
+// of key/value pairs.
+//
+fn map(filename: &str, contents: &str) -> Vec<KVStore<&str, &str>> {
+    let mut words = contents.split(" ");
+    
+    let mut kva: Vec<KVStore<&str, &str>> = Vec::new();
+    for word in words {
+        kva.push(KVStore::new(word , "1"));
+    }
+
+    return kva
 }
